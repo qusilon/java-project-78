0a020c2fb5790f191b317a9d2e72c6ce90834b75
Alexandr Pershin <aleks98_06@mail.ru>
2025-04-24 21:33:03 +0700

    add NumberSchema

--- app/src/main/java/hexlet/code/schemas/NumberSchema.java
+++ app/src/main/java/hexlet/code/schemas/NumberSchema.java
@@ -1,5 +1,37 @@
 package hexlet.code.schemas;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Predicate;
+
 public class NumberSchema {
 
+    private Map<String, Predicate<Integer>> limitations = new HashMap<>();
+
+    public NumberSchema required() {
+        limitations.put("required", content -> content == null || content.equals(""));
+        return this;
+    }
+
+    public NumberSchema positive() {
+        limitations.put("positive", content -> content == null || content <= 0);
+        return this;
+    }
+
+    public NumberSchema range(int first, int end) {
+        limitations.put("range", content -> content < first || content > end);
+        return this;
+    }
+
+    public boolean isValid(Integer content) {
+        if (content == null && !limitations.containsKey("required")) {
+            return true;
+        }
+        for (Predicate<Integer> limitation : limitations.values()) {
+            if (limitation.test(content)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- app/src/test/java/hexlet/code/ValidatorTest.java
+++ app/src/test/java/hexlet/code/ValidatorTest.java
@@ -1,5 +1,6 @@
 package hexlet.code;
 
+import hexlet.code.schemas.NumberSchema;
 import hexlet.code.schemas.StringSchema;
 import org.junit.jupiter.api.Test;
 
@@ -31,5 +32,29 @@ public class ValidatorTest {
 
         var schema1 = v.string();
         assertTrue(schema1.minLength(10).minLength(4).isValid("Hexlet"));
+    }
+
+    @Test
+    public void testNumberValidator() {
+        var v = new Validator();
+        NumberSchema schema = v.number();
+
+        assertTrue(schema.isValid(null));
+        assertTrue(schema.positive().isValid(null));
+
+        schema.required();
+
+        assertFalse(schema.isValid(null));
+        assertTrue(schema.isValid(10));
+
+        assertFalse(schema.isValid(-10));
+        assertFalse(schema.isValid(0));
+
+        schema.range(5, 10);
+
+        assertTrue(schema.isValid(5));
+        assertTrue(schema.isValid(10));
+        assertFalse(schema.isValid(4));
+        assertFalse(schema.isValid(11));
     }
 }
